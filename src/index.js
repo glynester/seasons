1743f764518bc329fdc67d64b033f3872de30e18
Gee <glynester@hotmail.com>
2022-03-01 15:41:19 +0000

    Reactored App function to a class

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import react from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-const App=()=>{
-  return <div>Hi there!</div>
-
+class App extends React.Component {
+  render(){
+    window.navigator.geolocation.getCurrentPosition(
+      (position)=> console.log(position),
+      (err)=> console.log(err)
+    ); 
+    return <div>Lattitude: </div>
+  }
 }
+
+// Can’t make this asynchronous with functional component
+// const App=()=>{
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position)=> console.log(position),
+//     (err)=> console.log(err)
+//   );  
+//   return <div>Lattitude: </div>
+// }
 
 ReactDOM.render(
   <App />,
