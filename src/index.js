be78d0aee062cd098b4a65c000ea366b2b5aa04f
Gee <glynester@hotmail.com>
2022-03-01 18:38:50 +0000

    Error handling and conditional rendering

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   // First function called when class instance is created.
   constructor(props){
     super(props);  // Calls parent's (React.Component) constructor function. Must be done every time. This is the only time we do direct assignment to this.state!!!
-    this.state={ lat: null };   // Can now be referenced by any function inside of App component.
+    this.state={ lat: null, errorMessage: '' };   // Can now be referenced by any function inside of App component.
     window.navigator.geolocation.getCurrentPosition(
       (position)=> {
         console.log(position);
@@ -13,13 +13,28 @@ class App extends React.Component {
         // Do NOT do it any other way like: this.state.lat =  position.coords.latitude
         this.setState({lat:position.coords.latitude});  // setState comes from React.Component
       },
-      (err)=> console.log(err)
+      (err)=> {
+        console.log(err);
+        this.setState({ errorMessage: err.message })
+      }
     ); 
   }
 
   // React says we have to define render!!
   render(){
-    return <div>Latitude: {this.state.lat}</div>
+    if (this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>
+    } 
+    if (!this.state.errorMessage && this.state.lat){
+      return <div>Latitude: {this.state.lat} </div>
+    } 
+    return <div>Loading...</div>
+    // return (
+    //   <div>
+    //     Latitude: {this.state.lat} <br/>
+    //     Error: {this.state.errorMessage}
+    //   </div>
+    // );
   }
 }
 
