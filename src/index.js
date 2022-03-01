40b753a9e1ad6b3494edd1fff94ff8b55ff1cd1b
Gee <glynester@hotmail.com>
2022-03-01 20:01:14 +0000

    Refactored geo into componentDidMount(). New way to set state and removed constructor.

--- src/index.js
+++ src/index.js
@@ -2,25 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  // First function called when class instance is created.
-  constructor(props){
-    super(props);  // Calls parent's (React.Component) constructor function. Must be done every time. This is the only time we do direct assignment to this.state!!!
-    this.state={ lat: null, errorMessage: '' };   // Can now be referenced by any function inside of App component.
+  // constructor(props){
+  //   super(props); 
+  //   this.state={ lat: null, errorMessage: '' };
+  // }
+
+  state={lat:null, errorMessage: '' };
+
+  componentDidMount(){
+    console.log("component did mount!!!");
     window.navigator.geolocation.getCurrentPosition(
-      (position)=> {
-        console.log(position);
-        // setState must be called to change the state
-        // Do NOT do it any other way like: this.state.lat =  position.coords.latitude
-        this.setState({lat:position.coords.latitude});  // setState comes from React.Component
-      },
-      (err)=> {
-        console.log(err);
-        this.setState({ errorMessage: err.message })
-      }
+      position=>this.setState({lat:position.coords.latitude}),
+      err=>this.setState({ errorMessage: err.message })
     ); 
   }
-
-  // React says we have to define render!!
+  componentDidUpdate(){
+    console.log("component rerendered!!!")
+  }
   render(){
     if (this.state.errorMessage && !this.state.lat){
       return <div>Error: {this.state.errorMessage}</div>
@@ -29,12 +27,6 @@ class App extends React.Component {
       return <div>Latitude: {this.state.lat} </div>
     } 
     return <div>Loading...</div>
-    // return (
-    //   <div>
-    //     Latitude: {this.state.lat} <br/>
-    //     Error: {this.state.errorMessage}
-    //   </div>
-    // );
   }
 }
 
